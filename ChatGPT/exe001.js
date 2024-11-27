0319ce32a7b1942980014c56a2a0936bc31090f6
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2024-11-27 16:54:54 -0300

    Update

---
+++ ChatGPT/exe001.js
@@ -0,0 +1,9 @@
+// Determinar maioridade
+// Escreva uma condição ternária para verificar se uma pessoa com idade armazenada na variável idade é maior de idade (18 anos ou mais). Retorne "Maior de idade" ou "Menor de idade".
+
+let idade = 15;
+if (idade >= 18) {
+    console.log('Maior de idade!');
+} else {
+    console.log('Menor de idade!');
+}
