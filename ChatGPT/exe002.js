74c8489cff337fadc835f36c8b70877cc1e8e6ff
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2024-11-28 01:41:06 -0300

    Update: exe002 e exe003 - ChatGPT

---
+++ ChatGPT/exe002.js
@@ -0,0 +1,9 @@
+// Número par ou ímpar
+// Verifique se um número armazenado na variável numero é par ou ímpar. Retorne "Par" ou "Ímpar".
+
+let numero = 10;
+if (numero % 2 == 0) {
+    console.log('Número Par');
+} else {
+    console.log('Número Ímpar');
+};
---
+++ ChatGPT/exe003.js
@@ -0,0 +1,9 @@
+// Verificar aprovação
+// Com base na nota de um aluno armazenada na variável nota, retorne "Aprovado" se a nota for maior ou igual a 7, ou "Reprovado" caso contrário.
+
+let nota = 5;
+if (nota >= 7) {
+    console.log('Aprovado');
+} else {
+    console.log('Reprovado');
+};
